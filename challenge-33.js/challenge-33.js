0db41e00fd5cc11e072ca9dcf0df205acacbb4ea
Rudimar Silva <70194689+rdmsilv@users.noreply.github.com>
2021-03-05 03:05:56 -0300

    Update challenge-33.js

--- challenge-33.js/challenge-33.js
+++ challenge-33.js/challenge-33.js
@@ -19,4 +19,4 @@ repositório do GitHub, e envie o pull request para lá.
 Depois, envie um pull request no repositório do curso, colocando no console.log
 abaixo a URL do pull request no seu repositório.
 */
-console.log('Link do pull request do seu projeto');
+console.log('https://github.com/rdmsilv/curso-de-js-ninja/tree/main/challenge-33.js');
